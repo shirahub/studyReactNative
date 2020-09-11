2600f647d5cb533ebf86f1718eb56a592e90a3ce
Shirleen G2 <shirleen.adriana@gmail.com>
2020-09-12 02:58:30 +0700

    implement userlist add, delete, search

--- TesterProject/App.js
+++ TesterProject/App.js
@@ -1,16 +1,75 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, TextInput, Button, Image } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button, Image, ScrollView, TouchableOpacity } from 'react-native';
 
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Appleen is the best app",
+      userlist: [{ "username": "shir", "password": "leen", "picture":'https://banner2.cleanpng.com/20180402/puq/kisspng-apple-color-emoji-fire-symbol-fire-letter-5ac1d6665a7469.5616020915226527743705.jpg' }],
+      searchlist: []
+    }
+  }
 
+  componentWillMount() {
+    this.setState({
+      searchlist:this.state.userlist
+    })
+  }
+  // changeTitle = (new) => {
+  //   this.setState({
+  //     title: new
+  //   })
+  // }
+  deleteUser = (username) => {
+    console.log(username)
+    for (var i = 0; i< this.state.userlist.length; i++) {
+      console.log(username === this.state.userlist[i].username)
+      if (username === this.state.userlist[i].username) {
+        this.state.userlist.splice(i, 1)
+      }
+    }
+    console.log("delete", this.state.userlist)
+  }
+
+  searchUser = (username) => {
+    console.log(username)
+    var templist = []
+    for (var i = 0; i< this.state.userlist.length; i++) {
+      if (this.state.userlist[i].username.includes(username) || !username) {
+        templist.push(this.state.userlist[i])
+      }
+    }
+    this.setState({
+      searchlist:templist
+    })
+    console.log("search", this.state.userlist)
+  }
+
+  render() {
+    return (
+      // <App title={this.state.title} changeTitle={title => this.setState({ title })}>
+      <App
+        userlist={this.state.userlist}
+        searchlist={this.state.searchlist}
+        addUser={user => this.state.userlist.push(user), this.searchUser}
+        deleteUser={this.deleteUser}
+        searchUser={this.searchUser}>
+      </App>
+    )
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      userlist: [{ "username": "shir", "password": "leen" }],
       username: '',
       password: '',
-      repassword: ''
+      repassword: '',
+      title: '',
+      picture: '',
+      search: '',
     }
   }
 
@@ -28,19 +87,23 @@ class App extends Component {
       return alert("Password (again) is required")
     }
 
+    if (!this.state.picture) {
+      return alert("Picture is required")
+    }
+
     if (!(this.state.password === this.state.repassword)) {
       return alert("Please input the same password")
     }
 
-    var newUser = { "username": this.state.username, "password": this.state.password }
+    var newUser = { "username": this.state.username, "password": this.state.password, "picture": this.state.picture }
 
-    for (var i = 0; i < this.state.userlist.length; i++) {
-      if (this.state.userlist[i].username === this.state.username) {
+    for (var i = 0; i < this.props.userlist.length; i++) {
+      if (this.props.userlist[i].username === this.state.username) {
         return alert("Username has been used")
       }
     }
 
-    this.state.userlist.push(newUser)
+    this.props.addUser(newUser)
     return alert("Registration success")
   }
 
@@ -54,44 +117,96 @@ class App extends Component {
       return alert("Password is required")
     }
 
-    for (var i = 0; i < this.state.userlist.length; i++) {
-      if (this.state.userlist[i].username === this.state.username && this.state.userlist[i].password === this.state.password) {
+    for (var i = 0; i < this.props.userlist.length; i++) {
+      if (this.props.userlist[i].username === this.state.username && this.props.userlist[i].password === this.state.password) {
         return alert("Login Success")
       }
     }
     return alert("Username/Password Invalid")
   }
 
+  searchBar = () => {
+    this.props.searchUser(this.state.search)
+  }
+
+  // handleChangeTitle = () => {
+  //   this.props.changeTitle("Appleen is not the best")
+  // }
+
+  // UNSAFE_componentWillReceiveProps(a) {
+  //   console.log("componentWillReceiveProps", a.title)
+  //   console.log(a.title !== "Appleen is still the best")
+  //   if (a.title !== "Appleen is still the best") {
+  //     this.setState({
+  //       title: "APPLEEN THE BEST"
+  //     })
+  //   }
+  // }
+
   render() {
     return (
       <>
-        <View style={styles.container}>
-          <Image style={styles.logo} source={{ uri: 'https://banner2.cleanpng.com/20180402/puq/kisspng-apple-color-emoji-fire-symbol-fire-letter-5ac1d6665a7469.5616020915226527743705.jpg' }} />
-          <Text style={styles.title}>APPLEEN</Text>
-          <View style={styles.form}>
-            <Text style={styles.subtitle}>
-              Register
+        <ScrollView>
+          <View style={styles.container}>
+            <Image style={styles.logo} source={{ uri: 'https://banner2.cleanpng.com/20180402/puq/kisspng-apple-color-emoji-fire-symbol-fire-letter-5ac1d6665a7469.5616020915226527743705.jpg' }} />
+            <Text style={styles.title}>APPLEEN</Text>
+            {/* <Text style={{ textAlign: "center" }}>{this.state.title}</Text>
+            <Button onPress={this.handleChangeTitle} title="Change" /> */}
+            <View style={styles.form}>
+              <Text style={styles.subtitle}>
+                Register
             </Text>
-            <Text>Input username</Text>
-            <TextInput style={styles.inputbox} onChangeText={username => this.setState({ username })} />
-            <Text>Input password</Text>
-            <TextInput style={styles.inputbox} secureTextEntry={true} onChangeText={password => this.setState({ password })} />
-            <Text>Input password (again)</Text>
-            <TextInput style={styles.inputbox} secureTextEntry={true} onChangeText={repassword => this.setState({ repassword })} />
-            <Button title="Register" onPress={this.registerNewUser}></Button>
+              <Text>Input username</Text>
+              <TextInput style={styles.inputbox} onChangeText={username => this.setState({ username })} />
+              <Text>Input picture</Text>
+              <TextInput style={styles.inputbox} onChangeText={picture => this.setState({ picture })} />
+              <Text>Input password</Text>
+              <TextInput style={styles.inputbox} secureTextEntry={true} onChangeText={password => this.setState({ password })} />
+              <Text>Input password (again)</Text>
+              <TextInput style={styles.inputbox} secureTextEntry={true} onChangeText={repassword => this.setState({ repassword })} />
+              <Button title="Register" onPress={this.registerNewUser}></Button>
+            </View>
+
+            <View style={styles.form}>
+              <Text style={styles.subtitle}>
+                Login
+            </Text>
+              <Text>Input username</Text>
+              <TextInput style={styles.inputbox} onChangeText={username => this.setState({ username })} />
+              <Text>Input password</Text>
+              <TextInput style={styles.inputbox} secureTextEntry={true} onChangeText={password => this.setState({ password })} />
+              <Button title="Login" onPress={this.loginUser}></Button>
+            </View>
           </View>
 
-          <View style={styles.form}>
-            <Text style={styles.subtitle}>
-              Login
-            </Text>
-            <Text>Input username</Text>
-            <TextInput style={styles.inputbox} onChangeText={username => this.setState({ username })} />
-            <Text>Input password</Text>
-            <TextInput style={styles.inputbox} secureTextEntry={true} onChangeText={password => this.setState({ password })} />
-            <Button title="Login" onPress={this.loginUser}></Button>
+          <View>
+            <Text>Search</Text>
+            <TextInput style={styles.inputbox} onChangeText={search => this.setState({ search })} />
+            <Button title="Search" onPress={this.searchBar}></Button>
           </View>
-        </View>
+
+          <View>
+            {
+              this.props.searchlist.map((user, index) => (
+                <View style={styles.list}>
+                <TouchableOpacity
+                  key={user.username}
+                  onPress={() => alert(user.username)}>
+                  <Text style={styles.text}>
+                    {user.username}
+                  </Text>
+                  
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => this.props.deleteUser(user.username)}>
+                  <Text>Delete</Text>
+                </TouchableOpacity>
+                <Image style={styles.picture} source={{uri:(user.picture)}} />
+                </View>
+               ))
+            }
+          </View>
+        </ScrollView>
       </>
     )
   }
@@ -102,9 +217,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  list: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
   logo: {
-    marginTop:20,
+    marginTop: 20,
     alignSelf: 'center',
+    width: 66,
+    height: 58,
+  },
+  picture: {
     width: 66,
     height: 58,
   },
@@ -130,134 +254,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default App
-// const HelloWorldApp = () => {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center"
-//       }}>
-//       <Text>Hello, world!</Text>
-//     </View>
-//   )
-// }
-// export default HelloWorldApp;
-
-
-
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow strict-local
-//  */
-
-// import React from 'react';
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   ScrollView,
-//   View,
-//   Text,
-//   StatusBar,
-// } from 'react-native';
-
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// const App: () => React$Node = () => {
-//   return (
-//     <>
-//       <StatusBar barStyle="dark-content" />
-//       <SafeAreaView>
-//         <ScrollView
-//           contentInsetAdjustmentBehavior="automatic"
-//           style={styles.scrollView}>
-//           <Header />
-//           {global.HermesInternal == null ? null : (
-//             <View style={styles.engine}>
-//               <Text style={styles.footer}>Engine: Hermes</Text>
-//             </View>
-//           )}
-//           <View style={styles.body}>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Step One</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-//                 screen and then come back to see your edits.
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>See Your Changes</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <ReloadInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Debug</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <DebugInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Learn More</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Read the docs to discover what to do next:
-//               </Text>
-//             </View>
-//             <LearnMoreLinks />
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView>
-//     </>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
-
-// export default App;
+export default Main
